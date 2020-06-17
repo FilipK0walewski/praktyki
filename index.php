1e9c6f64bf4409ec82c118485ec49052b44f6b93
FilipK0walewski <filipkowalewski00@gmail.com>
2020-06-17 17:50:46 +0200

    dodanie pliku css

--- index.php
+++ index.php
@@ -83,105 +83,125 @@
 
 <!DOCTYPE HTML>
 <html lang = "pl">
-<head>
-    <meta charset = "utf-8"/>
-    <meta http-equiv = "X-UA-Compatible" content = "IE = edge, chlrome = 1"/>
+    <head>
+        <meta charset = "utf-8"/>
+        <meta http-equiv = "X-UA-Compatible" content = "IE = edge, chlrome = 1"/>
 
-    <link rel="style.css" href="style.css">
-    <link rel="stylesheet" href="https://unpkg.com/purecss@2.0.3/build/pure-min.css" integrity="sha384-cg6SkqEOCV1NbJoCu11+bm0NvBRc8IYLRGXkmNrqUBfTjmMYwNKPWBTIKyw9mHNJ" crossorigin="anonymous">
-    <link rel="stylesheet" href="dist/flatpickr.css">
-    <link rel="stylesheet" href="dist/ie.css">
-    <link rel="stylesheet" href="dist/plugins/confirmDate/confirmDate.css">
-    <link rel="stylesheet" href="dist/plugins/monthSelect/style.css">
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="https://unpkg.com/purecss@2.0.3/build/pure-min.css" integrity="sha384-cg6SkqEOCV1NbJoCu11+bm0NvBRc8IYLRGXkmNrqUBfTjmMYwNKPWBTIKyw9mHNJ" crossorigin="anonymous">
+        <link rel="stylesheet" href="dist/flatpickr.css">
+        <link rel="stylesheet" href="dist/ie.css">
+        <link rel="stylesheet" href="dist/plugins/confirmDate/confirmDate.css">
+        <link rel="stylesheet" href="dist/plugins/monthSelect/style.css">
 
-    <title>Check weather on flatearth</title>
-</head>
-<body>
+        <title>Sprawdź pogodę</title>
+    </head>
+    <body>
+        <div class = "header">
 
-    <div id = "container">
-        <form method = "post">
-            
-            Data: <br /> <input placeholder = "yyyy-mm-dd" value="<?php
-                if (isset($_SESSION['d']))
-                {
-                    echo $_SESSION['d'];
-                    unset($_SESSION['d']);
-                }
-            ?>" class = "date" name = "ddate"/> <br />
+            <form method = "post" class = "pure-form">
+                <fieldset>    
+                    <input placeholder = "Data ..." value="<?php
+                        if (isset($_SESSION['d']))
+                        {
+                            echo $_SESSION['d'];
+                            unset($_SESSION['d']);
+                        }
+                    ?>" class = "date" name = "ddate"/> 
 
-            <?php
-                if (isset($_SESSION['dateError']))
-                {
-                    echo '<div class="error">'.$_SESSION['dateError'].'</div>';
-                    unset($_SESSION['dateError']);
-                }
-            ?>
+                    <input placeholder = "Twoje miasto" type="text" value="<?php
+                        if (isset($_SESSION['c']))
+                        {
+                            echo $_SESSION['c'];
+                            unset($_SESSION['c']);
+                        }
+                    ?>" name="city" /> 
 
-            Miasto: <br /> <input type="text" value="<?php
-                if (isset($_SESSION['c']))
-                {
-                    echo $_SESSION['c'];
-                    unset($_SESSION['c']);
-                }
-            ?>" name="city" /> <br />
+                    <button type="submit" class="pure-button">Sprawdź</button>
 
-            <?php
-                if (isset($_SESSION['cityError']))
-                {
-                    echo '<div class="error">'.$_SESSION['cityError'].'</div>';
-                    unset($_SESSION['cityError']);
-                }
-            ?>
 
-            <input type="submit" value="Sprawdź" />
+                </fieldset>
+            </form>
+        </div>
 
-            <?php
-                if(isset($_SESSION['OK']) && $_SESSION['OK'] == true) {
+        <br />
+        <div class = "container">
 
-                    
-                    $n = count($h);
+                <?php
 
-                    for($i = 0; $i < $n; $i++){
-
-                        $iconURL = "http://openweathermap.org/img/wn/".$icon[$i]."@2x.png";
-                        ?>
-                            <div id = "block">
-                                Godzina:<?php echo $h[$i]?><br />
-                                Temperatura minimalna: <?php echo $tempMin[$i]?><br />
-                                Temperatura maksymalna: <?php echo $tempMax[$i]?><br />
-                                Wilgotność: <?php echo $humidity[$i]?><br />
-                                Ciśnienie: <?php echo $pressure[$i]?><br />
-                                <img src = <?php echo $iconURL ?>>
-
-                                <br /><br />
-                            </div>
-
-                        <?php
+                    if (isset($_SESSION['OK']) && $_SESSION['OK'] == true) {
 
                         
+
+                        
+                        $n = count($h);
+
+                        for($i = 0; $i < $n; $i++){
+
+                            $iconURL = "http://openweathermap.org/img/wn/".$icon[$i]."@2x.png";
+                            ?>
+                                <div id = "block">
+                                    <div id = "leftBlock">
+                                        Godzina:<?php echo $h[$i]?><br />
+                                        Temperatura minimalna: <?php echo $tempMin[$i]?>&degC<br />
+                                        Temperatura maksymalna: <?php echo $tempMax[$i]?>&degC<br />
+                                        Wilgotność: <?php echo $humidity[$i]?>%<br />
+                                        Ciśnienie: <?php echo $pressure[$i]?>hpa
+                                    </div>
+                                    <div id = "rightBlock">
+                                        <img src = <?php echo $iconURL ?>>
+                                    </div>
+                                    <br /><br />
+                                </div>
+                                <br />
+
+                            <?php
+
+                            
+                        }
                     }
-                }
-            ?>
+                    else if (isset($_SESSION['dateError']))
+                    {
+                        echo '<div class="error">'.$_SESSION['dateError'].'</div>';
+                        unset($_SESSION['dateError']);
+                    }
+                    else if (isset($_SESSION['cityError'])){
+                        echo '<div class="error">'.$_SESSION['cityError'].'</div>';
+                        unset($_SESSION['cityError']);
+                    }
+                    else{
+                        ?>
+                            <p class = "t">Prognoza pogody na 5 dni</p>
+                        <?php
+                    }
 
-        </form>
-    </div>
+                    unset($_SESSION['OK']);
+
+                ?>
+
+            
+            </div>
+
+            <div class = "footer">
+                    
+            </div>
 
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/classlist/1.2.20171210/classList.min.js"></script>
-    <script src="./dist/flatpickr.js"></script>
-    <script src="./dist/plugins/rangePlugin.js"></script>
-    <script src="./dist/plugins/confirmDate/confirmDate.js"></script>
-    <script src="./dist/plugins/minMaxTimePlugin.js"></script>
-    <script src="./dist/plugins/monthSelect/index.js"></script>
-    <script src="./dist/plugins/scrollPlugin.js"></script>
-    <script src="./dist/plugins/weekSelect/weekSelect.js"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/classlist/1.2.20171210/classList.min.js"></script>
+            <script src="./dist/flatpickr.js"></script>
+            <script src="./dist/plugins/rangePlugin.js"></script>
+            <script src="./dist/plugins/confirmDate/confirmDate.js"></script>
+            <script src="./dist/plugins/minMaxTimePlugin.js"></script>
+            <script src="./dist/plugins/monthSelect/index.js"></script>
+            <script src="./dist/plugins/scrollPlugin.js"></script>
+            <script src="./dist/plugins/weekSelect/weekSelect.js"></script>
 
-  <script>
-    var fp = flatpickr(".date", {
+        <script>
+            var fp = flatpickr(".date", {
 
-    })
-  </script>
+            })
+        </script>
 
-</body>
+    </body>
 
 </html>
